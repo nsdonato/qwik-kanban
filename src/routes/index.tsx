d8d6863bc9e6c1a1d62b5e4bafff578c931339ba
Noelia Donato <noeliadonato@gmail.com>
2023-06-15 23:25:00 +0200

    feat: add tailwind integration

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,7 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 export default component$(() => {
   return (
     <>
-      <h1>Hi 👋</h1>
+      <h1 class="text-blue-400 text-5xl">Hi 👋</h1>
       <p>
         Can't wait to see what you build with qwik!
         <br />
